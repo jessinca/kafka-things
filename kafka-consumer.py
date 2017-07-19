d1b199aa580f9c4190abde8314bef53b23850742
Charles Martinot <charles.martinot@honestbee.com>
2017-07-19 17:07:21 +0800

    Add message count

--- kafka-consumer.py
+++ kafka-consumer.py
@@ -1,7 +1,12 @@
 import sys
+import signal
 import os
 from pykafka import KafkaClient, SslConfig
 
+
+def signal_handler(signal, frame):
+    print 'Consumed %s messages' % message_count
+    sys.exit(0)
 
 if len(sys.argv) < 3:
   print 'Usage : python kafka-consumer.py <broker> <topic>'
@@ -12,6 +17,7 @@ topic = sys.argv[2]
 cafile = os.getenv('KAFKA_CA_FILE')
 certfile = os.getenv('KAFKA_CERT_FILE')
 keyfile = os.getenv('KAFKA_PRIVATE_KEY')
+message_count = 0
 
 if cafile is not None and certfile is not None and keyfile is not None:
   config = SslConfig(cafile=cafile,
@@ -23,7 +29,10 @@ else:
 
 topic = client.topics[topic]
 
+signal.signal(signal.SIGINT, signal_handler)
+
 consumer = topic.get_simple_consumer()
 for message in consumer:
   if message is not None:
     print message.offset, message.value
+    message_count += 1
